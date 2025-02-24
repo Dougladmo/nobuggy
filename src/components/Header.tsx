d1d47a8b57e6ac9b32424a6727a62be3e03660e4
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2025-02-24 13:54:51 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
   return (
     <header
       id="header"
-      className={`fixed z-10 -translate-y-1 md:z-20 flex items-center justify-center w-screen pt-6 pb-4 text-white font-[manrope] transition-all duration-500 ${bgOpacity}`}
+      className={`fixed z-10 -translate-y-[1px] md:z-20 flex items-center justify-center w-screen pt-6 pb-4 text-white font-[manrope] transition-all duration-500 ${bgOpacity}`}
     >
       <nav id="header-nav" className="w-full">
         <ul className="flex flex-wrap w-full justify-center row-gap-2 px-5 text-center gap-y-2 gap-x-5 md:items-center md:gap-16">
