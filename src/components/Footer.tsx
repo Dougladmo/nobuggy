7da8f3cb097244a8e54bbac286245c338001341c
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2025-02-24 14:21:46 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
   return (
-    <div id="footer" className="font-[comfortaa] font-light z-20 flex flex-col items-center justify-center gap-3 text-white bg-[#0E0E0E] py-5">
-        <div className="text-sm">
+    <div id="footer" className="font-[comfortaa] mt-10 font-light z-20 flex flex-col items-center justify-center gap-3 text-white bg-[#0E0E0E] py-5">
+        <div className="text-sm flex items-center justify-center">
             <a href="#" className="pr-5 border-r border-white">
                 Políticas de privacidade
             </a>
@@ -10,7 +10,7 @@ const Footer = () => {
                 Termos de uso
             </a>
         </div>
-        <p className="text-base"> &copy; 2025 NoBuggy. Todos os direitos reservados.</p>
+        <p className="text-base text-center"> &copy; 2025 NoBuggy. Todos os direitos reservados.</p>
     </div>
   )
 }
